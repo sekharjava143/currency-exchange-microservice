a6eb5693bf21e80b5283ad45a4399247e14ea634
gyanks <riitdel@gmail.com>
2019-02-28 12:36:39 +0530

    Initial version of Currency conversion Microservice

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','INR',79,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR','INR',95,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'AUD','INR',25,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10005,'NEP','INR',90,0);
+
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10007,'DRM','INR',18,0);
+
+
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10008,'SGD','INR',32,0);
+
+
